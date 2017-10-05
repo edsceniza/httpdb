83faa6dcaeeaf8ce890e7f4500c7049d89344b8b
edsceniza <30281485+edsceniza@users.noreply.github.com>
2017-10-05 15:52:19 +0800

    Initial commit

---
+++ src/app/services/database.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/Rx';
+
+
+@Injectable()
+export class DatabaseService {
+
+  constructor(private http: Http) { }
+
+  saveData(data: object) {
+      return this.http.post('https://firstproject-c50c8.firebaseio.com/movchar.json', data);
+  }
+
+  getData() {
+      return this.http.get('https://firstproject-c50c8.firebaseio.com/movchar.json')
+      .map(
+        (response: Response) => {
+            const data = response.json();
+            return data;
+        }
+      )
+      .catch(
+        (error: Response) => {
+            return Observable.throw('There was something wrong in the retrieval of the data...');
+        }
+      );
+  }
+
+}
