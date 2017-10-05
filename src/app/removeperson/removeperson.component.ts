73bbf009b74eedfa57328d5e9c3e92e5d77e6dd5
edsceniza <30281485+edsceniza@users.noreply.github.com>
2017-10-05 16:11:12 +0800

    Update

--- src/app/removeperson/removeperson.component.ts
+++ src/app/removeperson/removeperson.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { DbService } from '../services/db.service';
+import { IPerson } from '../interfaces/iperson';
+
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-removeperson',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./removeperson.component.css']
 })
 export class RemovepersonComponent implements OnInit {
+  @Output() modifyClicked = new EventEmitter<any>();
 
-  constructor() { }
+  baseURL = 'https://firstproject-c50c8.firebaseio.com';
+  remove:boolean = true;
+  rootNode = 'people';
+
+  peopleCollection: Array<IPerson> = [];
+
+  constructor(private dbservice: DbService) { }
 
   ngOnInit() {
+    this.loadData();
+    this.modifyClicked.emit(true);
   }
 
+  loadData() { 
+     this.dbservice.getAllData(`${this.baseURL}/${this.rootNode}.json`)
+     .subscribe(
+       (response) => {
+         this.peopleCollection = response;
+        } ,
+       (error) => console.log(error)
+     );
+  }
+
+  delData(dataID) {
+    console.log(this.baseURL);console.log(this.rootNode);console.log(dataID);
+    this.modifyClicked.emit(dataID);
+    this.dbservice.getID(`https://firstproject-c50c8.firebaseio.com/movchar/movchar.json`)
+
+    if(confirm("Would you like to delete this record?")==true)
+      if (confirm("Are you sure?")==true)
+        {
+          alert("Deleted! Change tabs to refresh");
+          this.dbservice.deleteData(`${this.baseURL}/${this.rootNode}/${dataID}.json`).subscribe(
+            (response) => console.log(response),
+            (error) => console.log(error)
+          );
+        }
+      else alert("Delete cancelled");
+      else alert("Delete cancelled");
+  }
 }
